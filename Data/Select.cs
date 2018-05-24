e61189faf8f480d8af2c4cfdea06b4a5f96fdb37
GET Academy <terje@kolderup.net>
2018-05-24 14:37:44 +0200

    x

---
+++ Data/Select.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+
+namespace SelectFromDb.Data
+{
+    public class Select<TDbModel> where TDbModel : new()
+    {
+        private readonly PropertyInfo[] _properties;
+
+        public Select()
+        {
+            var dbModelType = typeof(TDbModel);
+            _properties = dbModelType.GetProperties();
+        }
+
+        public TDbModel GetStudent(SqlConnection connection, int id)
+        {
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "Select Id, Navn From Student Where Id = @id";
+                command.Parameters.AddWithValue("id", id);
+                return ReadToList(command).FirstOrDefault();
+            }
+        }
+
+        public List<TDbModel> GetAllStudents(SqlConnection connection)
+        {
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "Select Id, Navn From Student";
+                return ReadToList(command);
+            }
+        }
+
+        private List<TDbModel> ReadToList(SqlCommand command)
+        {
+            var list = new List<TDbModel>();
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    var rowEntity = new TDbModel();
+                    ReadAllFields(reader, rowEntity);
+                    list.Add(rowEntity);
+                }
+            }
+            return list;
+        }
+
+        private void ReadAllFields(SqlDataReader reader, TDbModel rowEntity)
+        {
+            foreach (var property in _properties)
+            {
+                property.SetValue(rowEntity, reader[property.Name]);
+            }
+        }
+    }
+}
--- Data/SelectStudent.cs
+++ Data/SelectStudent.cs
@@ -1,47 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Linq;
-using SelectFromDb.DbModels;
+﻿using SelectFromDb.DbModels;
 
 namespace SelectFromDb.Data
 {
-    public class SelectStudent
+    public class SelectStudent : Select<Student>
     {
-        public Student GetStudent(SqlConnection connection, int id)
-        {
-            using (var command = connection.CreateCommand())
-            {
-                command.CommandText = "Select Id, Navn From Student Where Id = @id";
-                command.Parameters.AddWithValue("id", id);
-                return ReadToList(command).FirstOrDefault();
-            }
-        }
-
-        public List<Student> GetAllStudents(SqlConnection connection)
-        {
-            using (var command = connection.CreateCommand())
-            {
-                command.CommandText = "Select Id, Navn From Student";
-                return ReadToList(command);
-            }
-        }
-
-        private static List<Student> ReadToList(SqlCommand command)
-        {
-            var list = new List<Student>();
-            using (var reader = command.ExecuteReader())
-            {
-                while (reader.Read())
-                {
-                    list.Add(new Student()
-                    {
-                        Id = Convert.ToInt32(reader["Id"]),
-                        Navn = reader["Navn"].ToString()
-                    });
-                }
-            }
-            return list;
-        }
     }
 }
