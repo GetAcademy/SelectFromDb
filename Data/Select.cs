9d65f0b26f4aacb8250ae93f868b499a355f2a9d
GET Academy <terje@kolderup.net>
2018-05-24 13:53:23 +0200

    Initial commit

---
+++ Data/Select.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using SelectFromDb.DbModels;
+
+namespace SelectFromDb.Data
+{
+    public class Select
+    {
+        public Student GetStudent(SqlConnection connection, int id)
+        {
+            var list = new List<Student>();
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "Select Id, Navn From Student Where Id = @id";
+                command.Parameters.AddWithValue("id", id);
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new Student()
+                        {
+                            Id = Convert.ToInt32(reader["Id"]),
+                            Navn = reader["Navn"].ToString()
+                        });
+                    }
+                }
+            }
+            return list.FirstOrDefault();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using SelectFromDb.Data;
+
+namespace SelectFromDb
+{
+    class Program
+    {
+        static string ConnectionString => ConfigurationManager.ConnectionStrings["TestDb"].ConnectionString;
+
+        static void Main(string[] args)
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                var selectStudent = new Select();
+                var student = selectStudent.GetStudent(connection, 2);
+                Console.WriteLine(student);
+                Console.ReadKey();
+            }
+        }
+    }
+}
